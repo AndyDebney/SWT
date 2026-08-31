00beb7d2e211330d383e462ab8e6d22fa19d289a
Andy Debney <andy.debney@dbtlabs.com>
2026-08-31 22:39:23 +0000

    Sync6

---
+++ models/intermediate/int_staffs.sql
@@ -0,0 +1,14 @@
+select
+    _file as "source_file",
+    _line as "record_line_no",
+    _modified as "updated_at",
+    _fivetran_synced as "loaded_at",
+    staff_id,
+    first_name,
+    last_name,
+    email,
+    phone,
+    active,
+    store_id,
+    manager_id
+from {{ ref('stg_staffs') }}
