20b129822fa71cf50d1825edf043b1aa6e2b85d9
Andy Debney <andy.debney@dbtlabs.com>
2026-08-31 22:32:44 +0000

    sync 4

--- models/staging/stg_brands.sql
+++
@@ -1,8 +0,0 @@
-select
-    _file as "source_file",
-    _line as "record_line_no",
-    _modified as "updated_at",
-    _fivetran_synced as "loaded_at",
-    brand_id as "brand_id",
-    brand_name as "brand_name"
-from {{ source('raw', 'brands') }}
